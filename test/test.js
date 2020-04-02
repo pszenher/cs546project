4a1cc148c5c0b66816b559da30b66a70eb60cf33
Paul Szenher <pszenher@stevens.edu>
2020-04-02 12:43:00 -0400

    Implement unit tests for comment collection database functions

--- test/test.js
+++ test/test.js
@@ -25,3 +25,39 @@ describe("Sanity Check", function () {
     });
   });
 });
+
+// Database Method Unit Tests
+describe("Database Methods", function () {
+  describe("Comments", function () {
+    describe("addComment()", function () {
+      it("should return the added comment object when passed ObjectId types", async function () {
+        newComment = await data.comments.addComment(
+          ObjectId(),
+          ObjectId(),
+          "This is a comment"
+        );
+        expect(newComment).to.have.keys("_id", "songId", "userId", "content");
+      });
+      it("should have return comment with passed songId", async function () {
+        songId = ObjectId();
+        userId = ObjectId();
+        newComment = await data.comments.addComment(
+          songId,
+          userId,
+          "This is a comment"
+        );
+        expect(newComment.songId.toString()).to.equal(songId.toString());
+      });
+      it("should have return comment with passed userId", async function () {
+        songId = ObjectId();
+        userId = ObjectId();
+        newComment = await data.comments.addComment(
+          songId,
+          userId,
+          "This is a comment"
+        );
+        expect(newComment.userId.toString()).to.equal(userId.toString());
+      });
+    });
+  });
+});
