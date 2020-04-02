f7f0048741b040b868198dabf38d0500627ae95d
Paul Szenher <pszenher@stevens.edu>
2020-04-02 12:42:41 -0400

    Implement route functions for comments collection

---
+++ routes/comments.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const router = express.Router();
+
+const data = require("../data");
+const commentData = data.comments;
+
+router.post("/", async (req, res) => {
+  const newCommentData = req.body;
+  try {
+    if (!newCommentData.songId)
+      throw new Error("Commment post request requires 'songId' parameter");
+    if (!newCommentData.userId)
+      throw new Error("Commment post request requires 'userId' parameter");
+    if (!newCommentData.content)
+      throw new Error("Commment post request requires 'content' parameter");
+
+    if (typeof newCommentData.songId !== "string")
+      throw new TypeError(
+        "Expected string type for 'songId' parameter, got " +
+          typeof newCommentData.songId
+      );
+    if (typeof newCommentData.userId !== "string")
+      throw new TypeError(
+        "Expected string type for 'userId' parameter, got " +
+          typeof newCommentData.userId
+      );
+    if (typeof newCommentData.content !== "string")
+      throw new TypeError(
+        "Expected string type for 'content' parameter, got " +
+          typeof newCommentData.content
+      );
+
+    if (newCommentData.content.length > 500)
+      throw new Error(
+        "Comment string must be at max 500 characters, got length " +
+          newCommentData.content.length
+      );
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+    return;
+  }
+
+  try {
+    const { songId, userId, content } = newCommentData;
+    const newComment = await commentData.addComment(songId, userId, content);
+    res.json(newComment);
+  } catch (e) {
+    res.status(500).json({ error: e.toString() });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    if (!id) throw new Error("Commment post request requires 'id' parameter");
+
+    if (typeof id !== "string")
+      throw new TypeError(
+        "Expected string type for 'id' parameter, got " + typeof id
+      );
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+    return;
+  }
+
+  try {
+    const comment = await commentData.getCommentById(id);
+    res.json(comment);
+  } catch (e) {
+    res.status(404).json({ message: "Comment with id '" + id + "' not found" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  res.status(500).json({ error: "Not implemented" });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
+const commentRoutes = require("./comments");
+
 const constructorMethod = app => {
+  app.use("/comments", commentRoutes);
+
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Page not found" });
   });
