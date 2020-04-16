7cc023433e5215b02d892aaf49644d5d605aa6b0
Paul Szenher <pszenher@stevens.edu>
2020-04-16 13:48:54 -0400

    Reorganize common.js and add express configuration for route testing

--- test/common.js
+++ test/common.js
@@ -1,17 +1,26 @@
+// Configure local chai variables and plugins
 const chai = require("chai");
-
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
-
 const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
-
+// configure chai function gloabls
+global.chai = chai;
 global.assert = chai.assert;
 global.expect = chai.expect;
 
+// Configure project function globals
 global.data = require("../data");
 global.routes = require("../routes");
 
+// Configure mongodb function globals
 global.mongoConnection = require("../config/mongoConnection");
 global.ObjectId = require("mongodb").ObjectId;
+
+// Configure express and routes
+const express = require("express");
+const app = express();
+app.use(express.json());
+routes(app);
+global.app = app;
